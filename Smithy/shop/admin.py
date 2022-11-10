79d884ca15953eb1fd9db9f19b9e319399a0d1b0
Vipin Yadav <vy80091@gmail.com>
2022-11-10 17:59:43 +0530

    adding product

--- Smithy/shop/admin.py
+++ Smithy/shop/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from . models import Product
 
 # Register your models here.
 # username smithy123
 # email-vy840013@gmail.com
 # pass-smithy@123
+
+
+@admin.register(Product)
+class ProductModelAdmid(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discounted_price', 'selling_price',
+                    'category', 'product_image']
--- Smithy/shop/models.py
+++ Smithy/shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('T', 'Tools'),
+    ('A', 'Agriculture'),
+    ('W', 'Weapones'),
+    ('U', 'Utensils'),
+
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
